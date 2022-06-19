78d09579e93565c365d93c7b02b8615a2936b592
alberted115 <aldy@itu.dk>
2022-06-19 14:08:26 +0200

    Made cage collect princesses

--- DragonsFirstPrincess/Assets/Scripts/Cage.cs
+++ DragonsFirstPrincess/Assets/Scripts/Cage.cs
@@ -7,12 +7,15 @@ public class Cage : MonoBehaviour
 
     private PointManager pointManager;
     private PrincessSpawnManager princessSpawnManager;
+    private GameObject spawnPoint;
+    [SerializeField]private float maxSpawnRadius;
     // Start is called before the first frame update
     void Start()
     {
         pointManager = GameObject.FindGameObjectWithTag("PointManager").GetComponent<PointManager>();
         princessSpawnManager = GameObject.FindGameObjectWithTag("PrincessSpawnManager").GetComponent<PrincessSpawnManager>();
-    }
+        spawnPoint=transform.GetChild(0).gameObject;
+}
 
     // Update is called once per frame
     void Update()
@@ -26,8 +29,16 @@ public class Cage : MonoBehaviour
         {
             pointManager.AddPoints(1);
             princessSpawnManager.SpawnPrincess();
-            Destroy(other.gameObject);
+            PutInCage(other.gameObject);
         }
       
+    }
+
+    private void PutInCage(GameObject princess)
+    {
+        Vector3 spawnVector = new Vector3(Random.Range(-2, 2), 0, Random.Range(-2, 2));
+
+        princess.transform.position = spawnPoint.transform.position + spawnVector.normalized * Random.Range(0, maxSpawnRadius);
+        princess.transform.parent = transform;
     }
 }
